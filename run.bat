bdeb8f4a6676327d7080d966e01c718e0f8f29fa
أوسم العباني <awsamalabani1168@gmail.com>
2026-05-21 21:53:03 +0200

    convert the project to maven

--- run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo =======================================
-echo Starting Reverse Logistics Portal...
-echo =======================================
-
-java -cp out com.logistics.Main
-pause
